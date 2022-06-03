63cfff338e1da3c490ba3a1c865fe19cec01029a
Mike Silva <mike.silva@grupoicts.com.br>
2022-06-03 13:45:22 -0400

    feat: cleaning code

--- src/utils/FieldValidations.ts
+++
@@ -1,6 +0,0 @@
-export const required = (fieldName: string, customMessage?: string) => (
-    { 
-        required: true, 
-        message: customMessage ? customMessage : `Campo ${fieldName} é obrigatório` 
-    }
-)
